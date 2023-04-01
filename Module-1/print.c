bb06329f82855bfa49e8dfcc5697e998a2c1b1ee
shariar17e <shariarhossain23@gmail.com>
2023-04-01 21:43:33 +0600

    new tab and new line

--- Module-1/print.c
+++ Module-1/print.c
@@ -2,6 +2,10 @@
 
 int main(){
 
-    printf("hello world");
+    printf("hello world\n");
+    printf("I am shahriar\n");
+    printf("Now I am learning c programming");
+
+    printf("Hey now going to new tab\t here is new tab");
     return 0;
 }
