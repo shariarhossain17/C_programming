71f532c6fe024eadc4b53994b76787904daeb30c
shariar17e <shariarhossain23@gmail.com>
2023-04-02 00:12:19 +0600

    get input from user

---
+++ Module-1/input.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // int num1;
+    // float f_num;
+    // char c_text;
+    // scanf("%d %f %c",&num1,&f_num,&c_text);
+    // printf("%d %.1f %c",num1,f_num,c_text);
+
+    // int num,num2;
+    // char p;
+    // scanf("%d%c %d%c",&num,&p,&num2,&p);
+    // printf("%d%% %d%%",num,num2);
+    int num, num2;
+    scanf("%da %db", &num, &num2);
+    printf("%da %db", num, num2);
+    return 0;
+}
---
+++ Module-1/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+    // int num,num2;
+    // char p;
+    // scanf("%d%t %d%t",&num,&p,&num2,&p);
+    // printf("%d%% %d%%",num,num2);
