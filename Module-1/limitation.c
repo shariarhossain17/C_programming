48ab3d6c49b02638bc24663ca5e092c136517d31
shariar17e <shariarhossain23@gmail.com>
2023-04-02 02:41:40 +0600

    double and long long

---
+++ Module-1/limitation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () 
+{
+
+    // int num = 10000000000;
+    // long long b = 1000000000000000000;
+    // printf("%lld",b);
+
+    float float_num = 1000.12321242413;
+
+    double float_num2 = 1000.1232124241312;
+    printf("%0.13lf",float_num2);
+    return 0;
+}
