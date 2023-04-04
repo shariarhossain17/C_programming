efe1cbddc3c0d472894872b921c8161841417892
shariar17e <shariarhossain23@gmail.com>
2023-04-04 23:46:50 +0600

    break

---
+++ Module-3/break.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 5; i++)
+    {
+        if (i == 5)
+        {
+            break;
+        }
+
+        printf("%d\n", i);
+    }
+    return 0;
+}
