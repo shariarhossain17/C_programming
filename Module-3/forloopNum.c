ad03f6bb0aa989fb57730acb9f283e2f9ff0ed3b
shariar17e <shariarhossain23@gmail.com>
2023-04-05 00:08:02 +0600

    reverse num

--- Module-3/forloopNum.c
+++ Module-3/forloopNum.c
@@ -5,7 +5,12 @@ int main(void)
 {
     int i;
 
-    for (i = 1; i <= 5; i++)
+    // for (i = 1; i <= 5; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    for (i = 5 - 1; i >= 1; i--)
     {
         printf("%d\n", i);
     }
