2a9565c4cd42799bc66f49ea3d1d4f15fa6968d4
shariar17e <shariarhossain23@gmail.com>
2023-04-09 15:05:01 +0600

    problem 3

--- Module-4/char.c
+++ Module-4/char.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 int main()
 {
+    char str;
 
+    scanf("%c", &str);
+
+    if (str >= 97 && str <= 122)
+    {
+        int ans = str - 32;
+        printf("%c", ans);
+    }
+
+    else
+    {
+        int ans = str + 32;
+        printf("%c", ans);
+    }
     return 0;
 }
